9473c9d0fa907016a82f95aaee1d0271c441aadf
jiangzihao <jzh123s@gmail.com>
2018-03-14 14:08:01 +0800

    feat(server): add module koa-rewrite

--- server/src/app.js
+++ server/src/app.js
@@ -2,16 +2,17 @@
 
 const Koa = require('koa')
 const path = require('path')
+const config = require('./config')
 const logger = require('./lib/logger')
 const router = require('./routes').prefix('/api')
+const rewrite = require('koa-rewrite')
 const koaStatic = require('koa-static')
 const bodyParser = require('koa-bodyparser')
 
 const app = new Koa()
-
 app.use(logger.loggerMiddleware)
 app.use(bodyParser())
-app.use(router.routes())
+app.use(rewrite(`/${config.base}/(.*)`, '/$1'))app.use(router.routes())
 app.use(koaStatic(path.resolve(__dirname, './static')))
 
 module.exports = app
